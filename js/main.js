f58992e6642415b49edd84e77a823ed0166c41ce
sherry84922 <sherry95jhw@gmail.com>
2022-01-18 20:38:23 +0800

    hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const menuBtn = document.querySelector(".menu-btn");
+const menuItems = document.querySelector(".menu-items");
 
 // main toggle
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("open");
+  menuItems.classList.toggle("open");
 });
